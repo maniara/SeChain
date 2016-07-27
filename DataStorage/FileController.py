d21db011597a259c927209da9dd7911691fe8a3e
soohwancho <soohwancho@Outlook.com>
2016-07-27 23:17:28 +0900

    .

---
+++ DataStorage/FileController.py
@@ -0,0 +1,38 @@
+def write(fileName,message):
+    f = open(fileName,'a')
+    f.write(message)
+    f.write('\n')
+    f.close()
+
+
+def read(fileName,key):
+
+
+
+    return 0
+
+
+def addTransaction(trx):
+    write(""
+
+    return 0
+
+
+def readTransaction(key):
+
+
+    return 0
+
+
+def disableNode(key):
+
+    return 0
+
+def addNode(nodeJson):
+
+    return 0
+
+
+def writeAllTransaction():
+
+    return 0
