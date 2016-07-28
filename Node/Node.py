b5f30224866f655efb54e0a8457bacf55d5dbcf0
park <desjin2002@naver.com>
2016-07-28 14:59:49 +0900

    Update Node Package

--- Node/Node.py
+++ Node/Node.py
@@ -1,2 +1,9 @@
-class Node(object):
-    ip_address = None
+
+
+class Node():
+
+    def __init__(self, ip_address):
+        self.is_disabled = False
+        self.ip_address = ip_address
+        self.public_key = ''
+        self.private_key = ''
--- Node/NodeController.py
+++ Node/NodeController.py
@@ -1,6 +1,49 @@
-def is_node(ip_address):
-    return True;
 
-def create_node(ip_address):
-    import Node
-    return Node()
+# create new node
+# return JSON OBJECT
+def get_node(ip_address):
+    import Node, json, os
+    from KeyGenerator import generation_key_pair
+    from DataStorage import FileController
+    gen_public_key, gen_private_key = generation_key_pair(2**256)
+
+    node = Node.Node(ip_address)
+    node.public_key = gen_public_key
+    node.private_key = gen_private_key
+
+    json_node = {
+        'is_disabled' : False,
+        'public_key' : node.public_key,
+        'private_key' : node.private_key,
+        'ip_address' : node.ip_address
+    }
+
+    new_json_node = json.dumps(json_node)
+
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    file_path = file_path[:-4] + 'DataBase'
+    path_info = file_path + '\NodeInfo.txt'
+
+    # add node to file
+
+    FileController.write(path_info, new_json_node)
+
+    return new_json_node
+
+# # Add node to file
+# def add_node(node):
+#     import os
+#
+#     new_line ='\n'
+#
+    file_path = os.path.abspath(os.path.dirname(__file__))
+    file_path = file_path[:-4] + 'DataBase'
+    path_info = file_path +'\NodeInfo.txt'
+#
+#     f = open(path_info, 'a')
+#     f.write(node)
+#     f.write(new_line)
+#
+#     return True
+
+
