885adef19befe5a9b2cb60af659fa273613a7549
Deokyoon Ko <Deokyoon Ko>
2016-07-28 14:47:15 +0900

    0728

--- DataStorage/FileController.py
+++ DataStorage/FileController.py
@@ -1,38 +1,50 @@
-def write(fileName,message):
-    f = open(fileName,'a')
+import os
+
+database_path = os.path.dirname(os.path.dirname(__file__)) + '\DataBase' + '\\'
+node_info_file = 'NodeInfo.txt'
+ledger_file = 'Ledger.txt'
+
+
+def write(file_name, message):
+    f = open(file_name,'a')
     f.write(message)
     f.write('\n')
     f.close()
 
 
-def read(fileName,key):
+def read_all_line(file_name):
+    f=open(file_name,'r')
+    line_list=[]
+    while True:
+        line = f.readline()
+        if not line : break
+        else : line_list.append(line)
+    f.close()
+    return line_list
 
 
-
-    return 0
-
-
-def addTransaction(trx):
-    write(""
-
-    return 0
+def add_transaction(trx):
+    write(database_path + ledger_file, trx)
 
 
-def readTransaction(key):
+def get_ip_list():
+
+    return ip_list
 
 
-    return 0
+def get_transaction_list():
+    line_list = read_all_line(database_path + ledger_file)
+    return line_list
 
 
-def disableNode(key):
+def get_node(ip_address):
+    import json
 
-    return 0
+    node_list = get_node_list()
+    for node_string in node_list:
+        node = json.loads(node_string)
+        if node.ip_address == ip_address:
+            return node_string
+        else :
+            continue
 
-def addNode(nodeJson):
-
-    return 0
-
-
-def writeAllTransaction():
-
-    return 0
