b3a00d0d0dfad5df0ee1e94a2acac74a7adc0612
park <desjin2002@naver.com>
2016-07-29 21:04:51 +0900

    Update View Transaction

--- Transaction/TransactionController.py
+++ Transaction/TransactionController.py
@@ -1,18 +1,16 @@
 from CryptoController import dataEncode
 from CryptoController import dataDecode
-from Node import JsonEncoder
 import json
 import Transaction
 
 
 def print_all_transaction():
     from DataStorage import FileController
-    import unicodedata
     transaction_list = FileController.get_transaction_list()
-    for tr in transaction_list:
 
+    for tr in transaction_list:
         load_list = json.loads(tr)
-        load_list['value'] = load_list['value'].encode('ascii')
+        load_list['value'] = load_list['value'].decode('string_escape')
         data = dataDecode(load_list['value'], load_list['senderpublickey'])
         value = json.loads(data)
         print "TimeStamp : " + load_list['timestamp'] + " SenderPubKey : " + load_list['senderpublickey']
@@ -22,6 +20,7 @@ def print_all_transaction():
 def create_transaction(public_key, private_key, target_ip, amount, msg):
     t = Transaction.Transaction(public_key, target_ip, amount, msg)
     t.dic['value'] = dataEncode(t.dic['value'], private_key)
-    t.dic['value'] = unicode(t.dic['value'], errors = 'ignore')
+    # t.dic['value'] = unicode(t.dic['value'], errors = 'ignore')
+    t.dic['value'] = t.dic['value'].encode('string_escape')
     data = json.dumps(t.dic)
     return data
