023ce80850469cf6d573a3baea1b45cb360f40bd
Deokyoon Ko <Deokyoon Ko>
2016-07-29 18:05:00 +0900

    0728

--- Transaction/TransactionController.py
+++ Transaction/TransactionController.py
@@ -7,12 +7,13 @@ import Transaction
 
 
 def print_all_transaction():
-    transaction_list = read_all_transactions()
+    from DataStorage import FileController
+    transaction_list = FileController.get_transaction_list()
     for tr in transaction_list:
         load_list = json.loads(tr)
-        data = dataDecode(load_list['value'], load_list['publickey'])
+        data = dataDecode(load_list['value'], load_list['senderpublickey'])
         value = json.loads(data)
-        print "TimeStamp : " + load_list['timestamp'] + " SenderPubKey : " + load_list['publickey']
+        print "TimeStamp : " + load_list['timestamp'] + " SenderPubKey : " + load_list['senderpublickey']
         print "SenderIP : " + value['senderip'] + " ReceiveIP : " + value['reveiverip'] + " Amount : " + value['amount'] + " Msg : " + value['msg']
 
 
